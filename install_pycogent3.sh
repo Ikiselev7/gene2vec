d581278559b9132abad24fbed03c6246cf810b6f
Petr Baev <perry322lol@gmail.com>
2019-06-28 01:22:25 +0300

    argparser fixed, comments added, minor fixes

--- install_pycogent3.sh
+++ install_pycogent3.sh
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
+HEAD
 #!/usr/bin/env bash
-=======
->>>>>>> a928192c3d8922c4e5a7c80c71bc073069d36e7b
 wget -O cogent3.zip https://bitbucket.org/pycogent3/cogent3/get/3c81d2765761.zip
 DONT_USE_CYTHON=1 pip install cogent3.zip
 rm cogent3.zip
